20029bed4f94cd3b315b3da1b2226ac392b9358a
ASanoV <serge.asanov@gmail.com>
2020-08-06 22:31:42 +0300

    push task34

--- src/task34/Main.java
+++ src/task34/Main.java
@@ -1,7 +1,14 @@
 package task34;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+
+        System.out.printf("Максимально число из трех = %d", x > y && x > z ? x : y > x && y > z ? y : z);
     }
 }
