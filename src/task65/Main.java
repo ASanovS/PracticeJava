3eea913d2a06c487f67dcd621459d024dabc3e96
ASanoV <serge.asanov@gmail.com>
2020-08-20 20:27:52 +0300

    fix task65

--- src/task65/Main.java
+++ src/task65/Main.java
@@ -7,23 +7,22 @@ import java.util.Scanner;
  * верно ли, что n^2 равно кубу суммы цифр числа n.
  */
 public class Main {
-    private static int sum;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         int square = number * number;
         int cube = number * number * number;
+        int sumNumbCube = 0;
 
-        System.out.printf("Верно ли, что квадрат числа n равен" +
-                "сумме цыфр n в кубе - %s", square == sumNumbCube(cube)? "true" : "false");
-    }
-
-    static int sumNumbCube(int num) {
-        if (num > 0) {
-            sum += num % 10;
-            sumNumbCube(num /10);
+        while (cube > 0) {
+            sumNumbCube += cube % 10;
+            cube = cube / 10;
         }
-        return sum;
+
+        if (square == sumNumbCube) {
+            System.out.println("Квадрат числа n равен сумме цыфр n в кубе");
+        } else {
+            System.out.println("Квадрат числа n не равен сумме цыфр n в кубе");
+        }
     }
 }
