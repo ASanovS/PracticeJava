001e0b487726c13aa4b8aaaff6e092fd5da58aaf
ASanoV <serge.asanov@gmail.com>
2020-08-28 12:12:28 +0300

    fix 88 a

--- src/task88/MainA.java
+++ src/task88/MainA.java
@@ -3,13 +3,9 @@ package task88;
 public class MainA {
     public static boolean numSquare(int num) {
         int square = num * num;
-        int numberThree = 0;
         while (square > 0) {
             if (square % 10 == 3) {
-                numberThree++;
-                if (numberThree > 0) {
-                    return true;
-                }
+                return true;
             }
             square /= 10;
         }
