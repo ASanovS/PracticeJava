42ead39617b5dfdee4a5c911397fe9fe2a890632
ASanoV <serge.asanov@gmail.com>
2020-10-06 12:15:56 +0300

    add verify number scanner side

--- src/Shapes/Main.java
+++ src/Shapes/Main.java
@@ -12,32 +12,41 @@ public class Main {
         switch (shapes) {
             case TRIANGLE -> {
                 System.out.println("Введите стороны треугольника а, б, ц");
-                Triangle triangle = new Triangle(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble());
+                Triangle triangle = new Triangle(scannerSide(scanner), scannerSide(scanner), scannerSide(scanner));
                 System.out.println("Вы создали треугольник");
                 System.out.println("Площадь фигуры = " + triangle.calculateArea());
                 System.out.println("Периметр фигуры = " + triangle.calculatePerimeter());
             }
             case CIRCLE -> {
                 System.out.println("Введите радиус круга");
-                Circle circle = new Circle(scanner.nextDouble());
+                Circle circle = new Circle(scannerSide(scanner));
                 System.out.println("Вы создали круг");
                 System.out.println("Площадь фигуры = " + circle.calculateArea());
                 System.out.println("Периметр фигуры = " + circle.calculatePerimeter());
             }
             case SQUARE -> {
                 System.out.println("Введите длинну стороны квадрата");
-                Square square = new Square(scanner.nextDouble());
+                Square square = new Square(scannerSide(scanner));
                 System.out.println("Вы создали квадрат");
                 System.out.println("Площадь фигуры = " + square.calculateArea());
                 System.out.println("Периметр фигуры = " + square.calculatePerimeter());
             }
             case RECTANGLE -> {
                 System.out.println("Введите длинну сторон прямоугольника а и б");
-                Rectangle rectangle = new Rectangle(scanner.nextDouble(), scanner.nextDouble());
+                Rectangle rectangle = new Rectangle(scannerSide(scanner), scannerSide(scanner));
                 System.out.println("Вы создали прямоугольник");
                 System.out.println("Площадь фигуры = " + rectangle.calculateArea());
                 System.out.println("Периметр фигуры = " + rectangle.calculatePerimeter());
             }
         }
+    }
+
+    private static double scannerSide(Scanner scanner) {
+        double nu = scanner.nextDouble();
+        if (nu <= 0) {
+            System.out.println("Введите значение выше 0");
+            return scannerSide(scanner);
+        }
+        return nu;
     }
 }
